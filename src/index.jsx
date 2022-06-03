8f52e20b18c79ca5a78d12ff278720cd4b83da2b
Jason Huang <sixthshifts@gmail.com>
2022-06-03 04:42:26 +0000

    chore: add eslint
    Add ESLint with the airbnb and plugin:react/recommended configs,
    along with the supporting import, jsx-a11y, react, and react-hooks
    plugins. Add an .eslintignore for build output and normalise the
    index.jsx entry point to satisfy the new rules.

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<></>, document.body);
+ReactDOM.render(<div />, document.body);
