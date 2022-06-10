7900bea065f87640e76da7b92b2f276f46952bdb
Jason Huang <sixthshifts@gmail.com>
2022-06-10 07:19:29 +0000

    Internalise react-hexgrid

---
+++ src/components/game/hex.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import Model from '../../model/hex';
+import { Context } from './board';
+
+const points = [
+  { q: 1, r: 0 },
+  { q: 0.5, r: Math.sqrt(3) / 2 },
+  { q: -0.5, r: Math.sqrt(3) / 2 },
+  { q: -1, r: 0 },
+  { q: -0.5, r: -Math.sqrt(3) / 2 },
+  { q: 0.5, r: -Math.sqrt(3) / 2 },
+];
+
+function Hex({ model }) {
+  const { hexToPixel } = useContext(Context);
+
+  const pixel = hexToPixel(model.coordinates);
+
+  return (
+    <g transform={`translate(${pixel.x}, ${pixel.y})`}>
+      <polygon points={points.map(({ q, r }) => (`${q}, ${r}`)).join(' ')} />
+    </g>
+  );
+}
+
+Hex.propTypes = {
+  model: PropTypes.instanceOf(Model).isRequired,
+};
+
+export default Hex;
