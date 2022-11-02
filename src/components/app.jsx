7e1138d1bbc1d2db9f5b538dc156f70efdf3f618
Jason Huang <sixthshifts@gmail.com>
2022-11-02 21:11:21 +0000

    chore: log app version to console
    Log the package version on mount via a useEffect in App, and reset the
    package version to 0.0.1.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import pkg from '../../package.json';
 import '../stylesheet.css';
 import { GameContextProvider } from './context';
 import Game from './game';
@@ -6,6 +7,10 @@ import ClueMenu from './menu/clue';
 import MapMenu from './menu/map';
 
 function App() {
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.info(`Running Decryptid version ${pkg.version}`);
+  }, []);
   return (
     <div className="flex flex-col justify-center w-full h-full bg-secondary touch-none">
       <div className="gap-8 items-center p-4 w-full h-full auto-rotate-invert">
