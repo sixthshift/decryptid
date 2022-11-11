3e74e0e43e344b8045933f7f79ece77fe0d5ad44
Jason Huang <sixthshifts@gmail.com>
2022-11-11 12:22:00 +0000

    fix(board): improve token placement on a hex
    Compute token positions based on how many tokens a hex holds rather
    than spreading them all evenly around a circle.
    
    - A single token is centred.
    - Two tokens sit side by side with none in the middle.
    - Three or more place one in the centre and the rest in a surrounding
      ring.
    
    Also widen the placement radius from 0.5 to 0.6 of the hex radius.

--- src/components/game/hex/tokens/index.jsx
+++ src/components/game/hex/tokens/index.jsx
@@ -7,14 +7,21 @@ import Token from './token';
 
 function Tokens({ model }) {
   const [{ ui: { radius } }] = useGameContext();
+  const tokens = [...model.structures, ...model.clues];
+  const positions = (() => {
+    if (tokens.length === 1) {
+      // If only 1 token, centre it
+      return [{ x: 0, y: 0 }];
+    } if (tokens.length === 2) {
+      // If there are 2 tokens, then put them side by side without any in the middle
+      return toPoints(tokens.length, radius * 0.6);
+    }
+    // For all else, place one in the middle, and the rest in a circle around it
+    return [{ x: 0, y: 0 }, ...toPoints(tokens.length - 1, radius * 0.6)];
+  })();
   return (
     <g>
-      {
-        zip(
-          [...model.clues, ...model.structures],
-          toPoints(model.clues.length + model.structures.length, radius * 0.5),
-        ).map(([token, { x, y }]) => (<Token key={JSON.stringify(token)} token={token} transform={`translate(${x}, ${y})`} />))
-      }
+      {zip(tokens, positions).map(([token, { x, y }]) => (<Token key={JSON.stringify(token)} token={token} transform={`translate(${x}, ${y})`} />))}
     </g>
   );
 }
